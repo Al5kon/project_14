82af4d7e4b6f2b8fdfe81b81ca74358fbaebbe10
Alexey Konkov <alkonkov@rambler.ru>
2019-11-09 22:16:18 +0100

    Add property select

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,21 @@
-// const router = require('express').Router();
-// const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 
-// const User = require('../models/user');
-
-// router.post(cookieParser);
-
 module.exports = ((req, res, next) => {
-  console.log(req.headers);
   const { authorization } = req.headers;
-  console.log(authorization);
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
-  console.log(token);
   let payload;
   try {
     payload = jwt.verify(token, 'some-key');
-    console.log(payload);
   } catch (err) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
-  console.log('авторизация');
-
   return next();
 });
