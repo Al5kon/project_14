f1bda00fd91ed134ca2f6578c7376fc7a41226f5
Alexey Konkov <alkonkov@rambler.ru>
2019-11-09 18:00:48 +0100

    Separation of user rights

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,22 +6,28 @@ const jwt = require('jsonwebtoken');
 
 // router.post(cookieParser);
 
-module.exports = (req, res, next) => {
+module.exports = ((req, res, next) => {
+  console.log(req.headers);
   const { authorization } = req.headers;
+  console.log(authorization);
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
+  console.log(token);
   let payload;
   try {
     payload = jwt.verify(token, 'some-key');
+    console.log(payload);
   } catch (err) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
-  next();
-};
+  console.log('авторизация');
+
+  return next();
+});
